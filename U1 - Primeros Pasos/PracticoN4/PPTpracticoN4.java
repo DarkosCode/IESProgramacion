e3e7cefd11fd412b7a73aed5baa69e1a3e9614c4
Darkos <francocicc7@gmail.com>
2025-05-22 22:18:34 +0000

    tp4 terminado con JOptionPane, aunque todavia por corregir detalles de la interfaz de OptionPane que no tengo dominados

--- U1 - Primeros Pasos/PracticoN4/PPTpracticoN4.java
+++ U1 - Primeros Pasos/PracticoN4/PPTpracticoN4.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class PPTpracticoN4 {
     public static void main(String[] a) {
-        Scanner teclado = new Scanner(System.in);
 
         // ? PIEDRA PAPEL O TIJERA
 
@@ -14,61 +13,67 @@ public class PPTpracticoN4 {
         int winsUsuario = 0;
         int empates = 0;
 
-        while ((winsComputadora <= 3) || (winsUsuario <= 3)) {
+        while (winsComputadora < 3 && winsUsuario < 3) {
+            // Se va a frenar el codigo cuando uno llegue a 3 victorias.
 
             // ! parte de eleccion del usuario
-            System.out.println("Ingrese piedra papel o tijera: ");
-            String eleccionUsuario = teclado.nextLine();
-
-            // ! parte de eleccion de la computadora
-            String eleccionComputadora = null;
-            int numeroComputadora = (int) (Math.random() * 3) + 1;
-            System.out.println(numeroComputadora);
-
-            if (numeroComputadora == 1) {
-                eleccionComputadora = "piedra";
-            } else if (numeroComputadora == 2) {
-                eleccionComputadora = "papel";
-            } else if (numeroComputadora == 3) {
-                eleccionComputadora = "tijera";
+            String eleccionUsuario = JOptionPane.showInputDialog(null, "Ingrese piedra papel o tijera:",
+                    "Piedra, Papel o Tijera", JOptionPane.QUESTION_MESSAGE);
+            if (eleccionUsuario == null) {
+                JOptionPane.showMessageDialog(null, "Juego cancelado");
+                return;
             }
+            if (eleccionUsuario.toLowerCase().equals("piedra") ||
+                    eleccionUsuario.toLowerCase().equals("papel") ||
+                    eleccionUsuario.toLowerCase().equals("tijera")) {
 
-            if (eleccionUsuario.toLowerCase().equals("piedra") || eleccionUsuario.toLowerCase().equals("papel")
-                    || eleccionUsuario.toLowerCase().equals("tijera")) {
+                // ! parte de eleccion de la computadora
+                String eleccionComputadora = null;
+                int numeroComputadora = (int) (Math.random() * 3) + 1;
+
+                if (numeroComputadora == 1) {
+                    eleccionComputadora = "piedra";
+                } else if (numeroComputadora == 2) {
+                    eleccionComputadora = "papel";
+                } else if (numeroComputadora == 3) {
+                    eleccionComputadora = "tijera";
+                }
+
+                JOptionPane.showMessageDialog(null,
+                        "El usuario eligió: " + eleccionUsuario.toUpperCase() +
+                                "\nLa computadora eligió: " + eleccionComputadora.toUpperCase(),
+                        "ELECCIONES", JOptionPane.INFORMATION_MESSAGE);
+
                 if (eleccionComputadora.equals(eleccionUsuario.toLowerCase())) {
-                    System.out.println("Empate!");
+                    JOptionPane.showMessageDialog(null, "¡Empate!");
                     empates++;
-                    System.out.println("Usuario: " + winsUsuario + " Computadora: " + winsComputadora);
-                } else if (eleccionComputadora.equals("piedra") && eleccionUsuario.toLowerCase().equals("tijera")) {
+                } else if ((eleccionComputadora.equals("piedra") && eleccionUsuario.toLowerCase().equals("tijera"))
+                        || (eleccionComputadora.equals("papel") && eleccionUsuario.toLowerCase().equals("piedra"))
+                        || (eleccionComputadora.equals("tijera") && eleccionUsuario.toLowerCase().equals("papel"))) {
                     winsComputadora++;
-                    System.out.println("El usuario eligio " + eleccionUsuario + " y la computadora "
-                            + eleccionComputadora + ". Victoria para la computadora!");
-                    System.out.println("Usuario: " + winsUsuario + " Computadora: " + winsComputadora);
-                } else if (eleccionComputadora.equals("piedra") && eleccionUsuario.toLowerCase().equals("papel")) {
+                    JOptionPane.showMessageDialog(null, "Victoria para la computadora!");
+                    JOptionPane.showMessageDialog(null,
+                            "Usuario: " + winsUsuario + "\nComputadora: " + winsComputadora, "MARCADOR",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else if ((eleccionComputadora.equals("piedra") && eleccionUsuario.toLowerCase().equals("papel"))
+                        || (eleccionComputadora.equals("papel") && eleccionUsuario.toLowerCase().equals("tijera"))
+                        || (eleccionComputadora.equals("tijera") && eleccionUsuario.toLowerCase().equals("piedra"))) {
                     winsUsuario++;
-                    System.out.println("El usuario eligio " + eleccionUsuario + " y la computadora "
-                            + eleccionComputadora + ". Victoria para el usuario!");
-                    System.out.println("Usuario: " + winsUsuario + " Computadora: " + winsComputadora);
-                } else if (eleccionComputadora.equals("papel") && eleccionUsuario.toLowerCase().equals("piedra")) {
-                    winsComputadora++;
-                    System.out.println("El usuario eligio " + eleccionUsuario + " y la computadora "
-                            + eleccionComputadora + ". Victoria para la computadora!");
-                    System.out.println("Usuario: " + winsUsuario + " Computadora: " + winsComputadora);
-                } else if (eleccionComputadora.equals("papel") && eleccionUsuario.toLowerCase().equals("tijera")) {
-                    winsUsuario++;
-                    System.out.println("El usuario eligio " + eleccionUsuario + " y la  computadora "
-                            + eleccionComputadora + ". Victoria para el usuario!");
-                    System.out.println("Usuario: " + winsUsuario + " Computadora: " + winsComputadora);
-                } else {
-                    System.out.println("Algo ingresado esta mal");
+                    JOptionPane.showMessageDialog(null, "Victoria para el usuario!");
+                    JOptionPane.showMessageDialog(null,
+                            ("Usuario: " + winsUsuario + "\nComputadora: " + winsComputadora),
+                            "MARCADOR", JOptionPane.INFORMATION_MESSAGE);
                 }
             } else {
-                System.out.println("El valor ingresado es incorrecto, debe ser piedra, papel o tijera");
-                break;
+                JOptionPane.showMessageDialog(null,
+                        "El valor ingresado es incorrecto, debe ser piedra, papel o tijera");
             }
-        }
 
-        System.out.println("Termino la partida! Hubieron " + empates + " empates. El usuario consiguio " + winsUsuario
-                + " victorias y la computadora consiguio " + winsComputadora);
+        }
+        JOptionPane.showMessageDialog(null,
+                "Termino la partida! Hubieron " + empates + " empates. El usuario consiguio "
+                        + winsUsuario + " victorias y la computadora consiguio " + winsComputadora + " victorias",
+                "RESULTADO DE LA PARTIDA:", JOptionPane.INFORMATION_MESSAGE);
+
     }
 }
