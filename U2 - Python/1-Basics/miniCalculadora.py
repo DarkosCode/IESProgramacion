2e117c96b7db6a6c90ac5cb8d78e5c5587207bee
Darkos <francocicc7@gmail.com>
2025-08-14 16:02:04 +0000

    cambiando carpetas y empezando con python

---
+++ U2 - Python/1-Basics/miniCalculadora.py
@@ -0,0 +1,8 @@
+
+## !
+numeroUno = input('Ingresa el primer numero ')
+operador = input('Ingresa el operador ')
+numeroDos = input('Ingresa el segundo numero ')
+
+operacion = numeroUno + operador + numeroDos
+print(f'La operacion {operacion} da como resultado: {eval(operacion)} ')
