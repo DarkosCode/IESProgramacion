a2a15ab6e50df3c724ece4bbfd4af06e1faddcc1
DarkosDaKing <francocicc7@gmail.com>
2025-10-09 18:01:30 +0000

    cambiando el nombre y organizando carpetas

--- U1 - Primeros Pasos/mainPrimerasPruebas.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;
-// El * importa todas las clases dentro de lang
-
-public class mainPrimerasPruebas {
-    public static void main(String[] a) {
-        System.out.println("Hello World");
-    }
-
-}
