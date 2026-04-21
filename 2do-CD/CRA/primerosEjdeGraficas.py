5893d6902a418d92d80f060352b7910e6f03169c
DarkosDaKing <francocicc7@gmail.com>
2026-04-21 01:41:27 +0000

    ejercicios de practicaProfII y comienzo con matplotlib en CRA

--- 2do-CD/CRA/primerosEjdeGraficas.py
+++ 2do-CD/CRA/primerosEjdeGraficas.py
@@ -2,3 +2,14 @@ import matplotlib.pyplot as plt
 
 #? Primeros ejercicios con librerias de MatPlotLiB
 
+#*1.1 Basicas
+ejeX = [1, 2, 3, 4]
+ejeY = [10, 20, 30, 40]
+
+plt.plot(ejeX, ejeY)
+plt.title("Primer Grafico")
+plt.show()
+
+#*Etiquetando Ejes
+plt.xlabel("Horizontal!")
+plt.ylabel("Vertical!")
