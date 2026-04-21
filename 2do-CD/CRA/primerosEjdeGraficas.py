18c828952d66f1dcdbd53b9139eabda1e8df4401
Franco Ciccarelli <francocicc7@gmail.com>
2026-04-21 16:44:02 +0000

    requeriments actualizado, ejercicios con matplotlib hechos, .gitignore tambien actualizado para que no cargue imagenes

--- 2do-CD/CRA/primerosEjdeGraficas.py
+++ 2do-CD/CRA/primerosEjdeGraficas.py
@@ -1,15 +1,60 @@
 import matplotlib.pyplot as plt
+#! ↓ para graficos tridimensionales
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 #? Primeros ejercicios con librerias de MatPlotLiB
-
 #*1.1 Basicas
-ejeX = [1, 2, 3, 4]
-ejeY = [10, 20, 30, 40]
-
-plt.plot(ejeX, ejeY)
-plt.title("Primer Grafico")
-plt.show()
+# ejeX = [1, 2, 3, 4, 5, 6, 7]
+# ejeY = [10, 15, 20, 25, 30, 29, 40]
 
 #*Etiquetando Ejes
-plt.xlabel("Horizontal!")
-plt.ylabel("Vertical!")
+# plt.xlabel("Duracion (horas)")
+# plt.ylabel("Presupuesto (millones)")
+
+# plt.title("Grafico de Presupuesto x Duracion de pelicula")
+# #? .plot() es para grafico de linea.
+# # plt.plot(ejeX, ejeY,label="imdbTop50", color="brown", linestyle="-")
+# plt.legend()
+
+# #? .scatter() es para graficos de dispersion
+# plt.scatter(ejeX, ejeY, color="blue")
+# plt.title("De dispersion")
+
+# #? añadir cuadriculas al grafico
+# plt.grid()
+
+# plt.subplot(1,2,1)
+# plt.plot(ejeX, ejeY)
+# plt.title("G1")
+
+# plt.subplot(1,2,2)    
+# plt.bar(ejeX, ejeY) #? Grafico de barras
+# plt.title("G2")
+
+#? .hist() para graficos historiogramas
+# datos = [1,2,2,2,3,3,4,4,4,4,5,5,6]
+# plt.hist(datos, bins=5,color="purple")
+# plt.title("Historiograma")
+
+#? .pie() para graficos de torta
+tamaños = [15,30,45,10]
+etiquetas = ['A', 'B','C','D']
+plt.pie(tamaños, labels=etiquetas, autopct='%1.1f%%') #autopct es para el formato del numero.
+plt.title("1er Grafico de torta")
+
+
+
+# #* Graficos TriDimensionales
+# fig = plt.figure(figsize=(9, 6)) #Crea la "hoja en blanco" y le asigna un tamaño
+# ax = fig.add_subplot(111, projection="3d")
+# y = np.random.rand(50)
+# x = np.random.rand(50)
+# z = np.random.rand(50)
+
+# ax.scatter(x,y,z) # type: ignore
+
+# plt.savefig('nombreDelGrafico.png')
+
+#* Mostrar el grafico en pantalla
+plt.show()
