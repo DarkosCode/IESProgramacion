fd1a1097658818b51af4899fa9249b01109d0f3e
Franco Ciccarelli <francocicc7@gmail.com>
2026-05-08 18:32:17 +0000

    ..

---
+++ 2do-CD/POAD/pruebasRandom.py
@@ -0,0 +1,8 @@
+# variable: Iterable[int, str] = [5, "h"]
+# print(variable)
+
+# def mediana(numeros: list[  ]) ->  :
+#     o: list[  vacío ] =  vacío
+#     if numeros:
+#         return o[len(o)  vacío 2] if len(o) % 2 \
+#             else (o[len(o)  vacío 2 - 1] + o[len(o)  vacío 2])  vacío 2
