8f8ceeeacdd61fa59afd925591816cf0785950c8
Franco Ciccarelli <francocicc7@gmail.com>
2026-05-14 18:07:45 +0000

    pandas y demas pruebas para Programacion

---
+++ 2do-CD/POAD/pruebas/datosYbds.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# bDePrueba = {
+#     "nombre": ["Juan", "Maria", "Pedro", "Ana", "Luis"],
+#     "edad": [25, 20, 22, 19, 23],
+#     "sexo": ["Masculino", "Femenino", "Masculino", "Femenino", "Masculino"],
+#     "calificación": [8.5, 9.2, 7.8, 9.5, 8.9],
+# }
+
+# df_manual = pd.DataFrame(bDePrueba)
+# print(df_manual) #! 'Crea' una columna con Id's
+
+df_csv = pd.read_csv("https://drive.google.com/uc?id=1sTzcEYZlwdSprPamS810YygkHEuQ9s7h", sep=";")
+print(df_csv)
+print(df_csv.shape) #! largo y ancho (cantidad de columnas y filas) de el .csv  
+
+mayorA35 = df_csv[df_csv['Edad'] > 40]
+print(mayorA35)
+print(mayorA35.shape)  
