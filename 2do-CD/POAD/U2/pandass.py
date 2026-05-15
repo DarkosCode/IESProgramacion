77a07256eff80e0f07336727b583b3b876b242d4
Franco Ciccarelli <francocicc7@gmail.com>
2026-05-15 00:03:13 +0000

    actualizacion del requeriments, practicas de seaborn, matplot, etc. para POAD

--- 2do-CD/POAD/U2/pandass.py
+++ 2do-CD/POAD/U2/pandass.py
@@ -11,9 +11,12 @@ import pandas as pd
 # print(df_manual) #! 'Crea' una columna con Id's
 
 df_csv = pd.read_csv("https://drive.google.com/uc?id=1sTzcEYZlwdSprPamS810YygkHEuQ9s7h", sep=";")
-print(df_csv)
-print(df_csv.shape) #! largo y ancho (cantidad de columnas y filas) de el .csv  
+# print(df_csv)
+# print(df_csv.shape, "\n") #! largo y ancho (cantidad de columnas y filas) de el .csv  
 
-mayorA35 = df_csv[df_csv['Edad'] > 40]
-print(mayorA35)
-print(mayorA35.shape)  
+# mayorA35 = df_csv[df_csv['Edad'] > 40]
+# print(mayorA35)
+# print(mayorA35.shape, "\n")  
+
+df_csv = df_csv.rename(columns={"Rubro": "Dedicacion"}) #! Renombrar columna
+# print(df_csv)
